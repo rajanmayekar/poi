185cc31f4acf3f4ba2397c67b6999c7395e82d7f
rajan <rajan.mayekar@ness.com>
2015-07-09 19:51:33 +0530

    Added gulp connect whihc will allow us to directly serve our html app

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var connect = require('gulp-connect');
 
 var paths = {
   sass: ['./scss/**/*.scss']
@@ -49,4 +50,12 @@ gulp.task('git-check', function(done) {
     process.exit(1);
   }
   done();
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'www',
+    // livereload: true,
+    port:2000
+  });
 });
